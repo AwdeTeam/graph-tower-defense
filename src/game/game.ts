548269d8bc846f6180b0908fa2ac11bcd5bb8668
AwdeDarkar <darkardengeno@gmail.com>
2020-12-19 12:23:28 -0500

    Refactor Grid out of Game

--- src/game/game.ts
+++ src/game/game.ts
@@ -2,7 +2,7 @@
  * game
  * ====================================================================================================
  *
- * description
+ * Core game object
  *
  * ----------------------------------------------------------------------------------------------------
  * 
@@ -14,11 +14,20 @@
 
 import * as ex from "excalibur"
 
+import {Grid} from "./grid"
+
 const defaultConfig = {
     display: {
         width: 800,
         height: 600,
     },
+    game: {
+        grid: {
+            width: 10,
+            height: 10,
+            squareSize: 50,
+        },
+    }
 }
 
 export class Game {
@@ -36,7 +45,11 @@ export class Game {
             height: this.config.display.height,
             canvasElement: this.canvas,
         })
-		this.grid = new Grid(this, 10, 10, 50)
+        this.grid = new Grid(
+            this.config.game.grid.width,
+            this.config.game.grid.height,
+            this.config.game.grid.squareSize
+        )
 		this.engine.add(this.grid)
     }
 
@@ -44,64 +57,4 @@ export class Game {
         console.log("Starting game")
         this.engine.start()
     }
-}
-
-export class Grid extends ex.ScreenElement {
-
-	// array of squares
-	squares: GridSquare[][]
-	sizeX: number
-	sizeY: number
-	gridSize: number
-	
-	constructor(game: Game, sizeX: number, sizeY: number, gridSize: number) {
-		super({ x: 0, y: 0 })
-		this.sizeX = sizeX
-		this.sizeY = sizeY
-		this.gridSize = gridSize
-		this.squares = []
-	}
-
-	onInitialize() {
-		// create list of gridsquares
-		for (let x = 0; x < this.sizeX; x++)
-		{
-			this.squares[x] = []
-			for (let y = 0; y < this.sizeY; y++)
-			{
-				let gs = new GridSquare(x, y, this.gridSize)
-				this.squares[x][y] = gs
-			}
-		}
-	}
-
-	draw(ctx: CanvasRenderingContext2D, delta: number) {
-		for (let x = 0; x < this.sizeX; x++)
-		{
-			for (let y = 0; y < this.sizeY; y++) 
-			{
-				this.squares[x][y].draw(ctx, delta)
-			}
-		}
-	}
-	
-}
-
-export class GridSquare extends ex.ScreenElement { 
-	x: number
-	y: number
-	gridSize: number
-	//entities: ??[]
-	
-	constructor(x: number, y: number, gridSize: number) {
-		super({x: x, y: y, width: gridSize, height: gridSize})
-		this.x = x
-		this.y = y
-		this.gridSize = gridSize
-	}
-
-	draw(ctx: CanvasRect, delta: number) {
-		ctx.strokeRect(this.x*this.gridSize, this.y*this.gridSize, this.gridSize, this.gridSize)
-	}
-	
 }
---
+++ src/game/grid.ts
@@ -0,0 +1,74 @@
+/*
+ * grid
+ * ====================================================================================================
+ *
+ * Gameplay grid
+ *
+ * ----------------------------------------------------------------------------------------------------
+ * 
+ * **Created**
+ *    2020-12-19
+ * **Author**
+ *    WildfireXIII
+ */
+
+import * as ex from "excalibur"
+
+export class Grid extends ex.ScreenElement {
+
+	// array of squares
+	squares: GridSquare[][]
+	sizeX: number
+	sizeY: number
+	gridSize: number
+	
+	constructor(sizeX: number, sizeY: number, gridSize: number) {
+		super({ x: 0, y: 0 })
+		this.sizeX = sizeX
+		this.sizeY = sizeY
+		this.gridSize = gridSize
+		this.squares = []
+	}
+
+	onInitialize() {
+		// create list of gridsquares
+		for (let x = 0; x < this.sizeX; x++)
+		{
+			this.squares[x] = []
+			for (let y = 0; y < this.sizeY; y++)
+			{
+				let gs = new GridSquare(x, y, this.gridSize)
+				this.squares[x][y] = gs
+			}
+		}
+	}
+
+	draw(ctx: CanvasRenderingContext2D, delta: number) {
+		for (let x = 0; x < this.sizeX; x++)
+		{
+			for (let y = 0; y < this.sizeY; y++) 
+			{
+				this.squares[x][y].draw(ctx, delta)
+			}
+		}
+	}
+	
+}
+
+export class GridSquare extends ex.ScreenElement { 
+	x: number
+	y: number
+	gridSize: number
+	
+	constructor(x: number, y: number, gridSize: number) {
+		super({x: x, y: y, width: gridSize, height: gridSize})
+		this.x = x
+		this.y = y
+		this.gridSize = gridSize
+	}
+
+	draw(ctx: CanvasRect, delta: number) {
+		ctx.strokeRect(this.x*this.gridSize, this.y*this.gridSize, this.gridSize, this.gridSize)
+	}
+	
+}
