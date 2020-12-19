b8596d4233804bf4c1210bcd2c6a2b3eb78e4046
Nathan Martindale <nathanamartindale@gmail.com>
2020-12-19 10:59:33 -0500

    Started on graph stuff

--- src/game/game.ts
+++ src/game/game.ts
@@ -26,7 +26,7 @@ export class Game {
     engine: ex.Engine
     canvas: HTMLCanvasElement
 
-    constructor (canvas: HTMLCanvasElement, config: any = defaultConfig) {
+    constructor(canvas: HTMLCanvasElement, config: any = defaultConfig) {
         console.log("Building game")
         this.config = config
         this.canvas = canvas
@@ -37,8 +37,50 @@ export class Game {
         })
     }
 
-    start () {
+    start() {
         console.log("Starting game")
         this.engine.start()
     }
+}
+
+export class Grid extends ex.ScreenElement {
+
+	// array of squares
+	squares: GridSquare[][]
+	size_x: number
+	size_y: number
+	
+	constructor(game: Game, size_x: number, size_y: number) {
+		this.size_x = size_x
+		this.size_y = size_y
+		
+	}
+
+	onInitialize() {
+		// create list of gridsquares
+		for (let x = 0; x < this.size_x; x++)
+		{
+			for (let y = 0; y < this.size_y; y++)
+			{
+				let gs = GridSquare(x, y)
+			}
+		}
+	}
+
+	draw(ctx, delta) {
+
+	}
+	
+}
+
+export class GridSquare { 
+	x: number
+	y: number
+	entities: ??[]
+	
+	constructor(x: number, y: number) {
+		self.x = x
+		self.y = y
+	}
+	
 }
