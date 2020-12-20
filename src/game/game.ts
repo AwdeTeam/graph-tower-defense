5c6b937fac78048b93f0667a0ab1d7c3dbf1d5e3
AwdeDarkar <darkardengeno@gmail.com>
2020-12-20 12:04:30 -0500

    Begin implementing key events

--- src/game/game.ts
+++ src/game/game.ts
@@ -91,6 +91,7 @@ export class Game {
         this.mouseDownHandler = this.mouseDownHandler.bind(this)
         this.mouseUpHandler = this.mouseUpHandler.bind(this)
         this.mouseMoveHandler = this.mouseMoveHandler.bind(this)
+        this.keyHoldHandler = this.keyHoldHandler.bind(this)
         this.setupHandlers()
     }
 
@@ -98,6 +99,7 @@ export class Game {
         this.engine.input.pointers.primary.on('down', this.mouseDownHandler)
         this.engine.input.pointers.primary.on('up', this.mouseUpHandler)
         this.engine.input.pointers.primary.on('move', this.mouseMoveHandler)
+        this.engine.input.keyboard.on('hold', this.keyHoldHandler)
     }
 
     mouseDownHandler(event: ex.Input.PointerDownEvent) {
@@ -110,6 +112,10 @@ export class Game {
 
     mouseMoveHandler(event: ex.Input.PointerMoveEvent) {
         this.grid.mouseMoveHandler(event)
+    }
+
+    keyHoldHandler(event: ex.Input.KeyEvent) {
+        this.activePlayer.keyHoldHandler(event)
     }
 
     start() {
